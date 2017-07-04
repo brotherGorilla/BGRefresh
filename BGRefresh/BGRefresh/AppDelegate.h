bf24029a9791d305ba055934ff3441acbf4bf8a4
王志兴 <wang.zhixing@trs.com.cn>
2017-07-04 12:32:05 +0800

    创建项目

---
+++ BGRefresh/BGRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BGRefresh
+//
+//  Created by 王志兴 on 2017/7/4.
+//  Copyright © 2017年 brotherGorilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BGRefresh/BGRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BGRefresh
+//
+//  Created by 王志兴 on 2017/7/4.
+//  Copyright © 2017年 brotherGorilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
